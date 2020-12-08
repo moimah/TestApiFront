f7ac246baed4fa89f10ee0cfddaf84efca267be5
moimah <moimah@outlook.com>
2020-12-08 19:00:54 +0100

    ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,6 @@ import {FormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    NgModule,
     NgbModule,
     AppRoutingModule,
     HttpClientModule,
